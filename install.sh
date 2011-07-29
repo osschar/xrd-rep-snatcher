cbba01a0fb0c65e1257c5d661f7bead8f549c918
Matevž Tadel <osschar@users.noreply.github.com>
2011-07-29 03:22:12 +0000

    Support running as a daemon -- now default (requires Proc::Daemon). Get install.sh and init.d script to almost functional level.

--- install.sh
+++ install.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-cp xrd-rep-snatcher.pl     /usr/bin
-cp xrd-rep-snatcher.rc     /etc/xrootd/
-cp init.d/xrd-rep-snatcher /etc/init.d
+getent group  xrootd >/dev/null || groupadd -r xrootd
+getent passwd xrootd >/dev/null || \
+       useradd -r -g xrootd -c "Xrootd runtime user" -s /sbin/nologin -d /etc/xrootd xrootd
+
+mkdir -p /etc/xrootd
+mkdir -p            /var/run/xrootd /var/log/xrootd
+chown xrootd:xrootd /var/run/xrootd /var/log/xrootd
+
+if [ ! -e /etc/sysconfig/xrootd ]; then
+   cat > /etc/sysconfig/xrootd <<FNORD
+export XROOTD_USER=xrootd
+FNORD
+fi
+
+cp xrd-rep-snatcher.pl          /usr/bin
+cp xrd-rep-snatcher.rc          /etc/xrootd/
+cp init.d/xrd-rep-snatcher      /etc/init.d
 cp logrotate.d/xrd-rep-snatcher /etc/logrotate.d
+
+chkconfig --add xrd-rep-snatcher
